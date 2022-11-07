86a458c16c3a511993cabcd27df959bbcdf5abb6
Sevengal <34574643+Sevengal@users.noreply.github.com>
2022-11-07 22:12:45 +0100

    feat: working language switcher

--- src/components/ui/language-switcher/LanguageSwitcher.tsx
+++ src/components/ui/language-switcher/LanguageSwitcher.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { Dropdown } from 'react-bootstrap';
-
 import styles from './LanguageSwitcher.module.scss';
 
 function LocaleSwitcher() {
@@ -14,22 +13,35 @@ function LocaleSwitcher() {
   );
 
   return (
-    <Dropdown className={styles['language-switcher']}>
-      <Dropdown.Toggle id="languageSwitcher">Dropdown Button</Dropdown.Toggle>
+    <>
+      <Dropdown className={styles['language-switcher']}>
+        <Dropdown.Toggle
+          variant="link"
+          id="languageSwitcher"
+          className={styles['language-switcher__toggle']}
+        >
+          {activeLocale}
+        </Dropdown.Toggle>
 
-      <Dropdown.Menu className={styles['language-switcher__options']}>
-        {unselectedLocales?.map((locale) => (
-          <div
-            key={`locale-${locale}`}
-            className={styles['language-switcher__option']}
-          >
-            <Link href={{ pathname, query }} as={asPath} locale={locale}>
-              {locale}
-            </Link>
-          </div>
-        ))}
-      </Dropdown.Menu>
-    </Dropdown>
+        <Dropdown.Menu className={styles['language-switcher__menu']}>
+          {unselectedLocales?.map((locale) => (
+            <div
+              className={styles['language-switcher__menu-option']}
+              key={`locale-${locale}`}
+            >
+              <Link
+                className={styles['language-switcher__link']}
+                href={{ pathname, query }}
+                as={asPath}
+                locale={locale}
+              >
+                {locale}
+              </Link>
+            </div>
+          ))}
+        </Dropdown.Menu>
+      </Dropdown>
+    </>
   );
 }
 
