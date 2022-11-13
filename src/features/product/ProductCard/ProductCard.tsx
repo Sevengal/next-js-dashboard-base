7722cea11987fa578a952eecb0b8b236a96e3361
Sevengal <34574643+Sevengal@users.noreply.github.com>
2022-11-13 21:56:51 +0100

    feat: working counter store

--- src/features/product/ProductCard/ProductCard.tsx
+++ src/features/product/ProductCard/ProductCard.tsx
@@ -3,14 +3,20 @@ import Image from 'next/image';
 import styles from './PorductCard.module.scss';
 import { NAVIGATION_ROUTES } from '@constants/navigation-routes';
 import Link from 'next/link';
+import useAppSelector from '@hooks/redux/use-app-selector';
+
+import { selectCount } from '@stores/counter/CounterSelectors';
 
 interface ProductCardProps {
   product: Product;
 }
 
 const ProductCard = ({ product }: ProductCardProps) => {
+  const count = useAppSelector(selectCount);
+
   return (
     <div className={styles['product-card']}>
+      count: {count}
       <Link href={NAVIGATION_ROUTES.product.getPath(product.id)}>
         <div className={styles['product-card__image-container']}>
           <Image
